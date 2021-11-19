a53618353227e79881f196760b24fee83bb423c1
mathchq <mathchq@gmail.com>
2021-11-19 10:56:17 +0800

    first init repo

---
+++ 00-start.py
@@ -0,0 +1,19 @@
+import os, pprint, re, sys, time
+import glob
+
+from datetime import datetime, timezone, timedelta 
+import dateutil
+
+from multiprocessing import Pool, freeze_support
+import threading
+import asyncio
+
+import pathlib
+import tqdm
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import mplfinance as fplt
+print("(imported datetime, os, pprint, re, sys, time)")
+
+pp = pprint.pprint
---
+++ 99-token.py
@@ -0,0 +1,5 @@
+import ccxt
+
+BINANCE_KILINE_NAME = ['open_time', 'open', 'high', 'low', 'close', 'volume', 'close_time',
+       'quote_asset_volume', 'num_of_trades', 'taker_buy_base_asset_volume',
+       'taker_buy_quote_asset_volume', 'ignore']
